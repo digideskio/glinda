470d76442060ed722d881fc07920bdbf5657514c
Will Monroe <futurulus@gmail.com>
2012-12-25 20:44:53 -0800

    Attribute getch

--- getch.py
+++ getch.py
@@ -1,3 +1,9 @@
+'''
+getch module by Danny Yoo on ActiveState Code Recipes.
+http://code.activestate.com/recipes/134892/
+'''
+
+
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
