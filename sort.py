29ae82bef125942865bd6f467022c4dc125c1e62
Will Monroe <futurulus@gmail.com>
2012-12-25 21:05:04 -0800

    Get rid of global variables

--- sort.py
+++ sort.py
@@ -1,60 +1,58 @@
 import random
-from collections import deque
 from getch import getch
 import math
 
 
-remaining = 0
-original = 0
+class Mergesorter(object):
+    def __init__(self, items):
+        self.items = items[:]
+        random.shuffle(self.items)
+        lg_n = math.ceil(math.log(len(self.items), 2))
+        self.remaining = self.original = len(self.items) * lg_n - 2 * lg_n + 1
 
+    def compare(self, left, right):
+        print
+        print '1. %s' % str(left)
+        print '2. %s' % str(right)
+        choice = ''
+        while True:
+            choice = getch()
+            if choice == 'q':
+                raise KeyboardInterrupt()
+            if choice and choice[0] in '12':
+                break
 
-def compare(left, right):
-	global remaining, original
+        self.remaining -= 1
+        print('at most %d remaining of %d (%f%%)' %
+              (self.remaining, self.original,
+               float(self.remaining)/self.original * 100))
+        return {'1': -1, '2': 1}[choice[0]]
 
-	print
-	print '1. %s' % str(left)
-	print '2. %s' % str(right)
-	choice = ''
-	while True:
-		choice = getch()
-		if choice == 'q':
-			raise KeyboardInterrupt()
-		if choice and choice[0] in '12':
-			break
+    def merge(self, left, right):
+        result = []
+        while left and right:
+            if self.compare(left[0], right[0]) <= 0:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
 
-	remaining -= 1
-	print 'at most %d remaining of %d (%f%%)' % (remaining, original, float(remaining)/original * 100)
-	return {'1': -1, '2': 1}[choice[0]]
+        result += left
+        result += right
+        return result
 
+    def sort(self, items=None):
+        if items is None:
+            items = self.items
 
-def merge(left, right):
-	result = []
-	while left and right:
-		if compare(left[0], right[0]) <= 0:
-			result.append(left[0])
-			left = left[1:]
-		else:
-			result.append(right[0])
-			right = right[1:]
-	
-	result += left
-	result += right
-	return result
-
-
-def mergesort_rec(items):
-	if len(items) <= 1:
-		return items
-	else:
-		return merge(mergesort_rec(items[:len(items)/2]),
-					 mergesort_rec(items[len(items)/2:]))
+        if len(items) <= 1:
+            return items
+        else:
+            return self.merge(self.sort(items[:len(items)/2]),
+                              self.sort(items[len(items)/2:]))
 
 
 def mergesort(items):
-	global remaining, original
-	
-	random.shuffle(items)
-	lg_n = math.ceil(math.log(len(items), 2))
-	remaining = original = len(items) * lg_n - 2 * lg_n + 1
-	return mergesort_rec(items)
+	return Mergesorter(items).sort()
 
